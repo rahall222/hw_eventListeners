d71d9e43c1bc63c7313330875471af5f2f0a0b48
Rebecca Hall <rahall222@gmail.com>
2015-02-28 16:22:14 +0000

    add event listener for submission of form

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 document.body.bgColor = "#2187B7";
 
+var myForm = document.getElementById( "myForm" );
+
+myForm.addEventListener("submit", function(){
+	var firstName = document.getElementById("firstName").value;
+	console.log("The firstName is: " + firstName);
+
+	var lastName = document.getElementById("lastName").value;
+	console.log("The lastName is: " + lastName);
+
+	var email = document.getElementById("email").value;
+	console.log("The email is: " + email);
+
+	var message = document.getElementById("message").value;
+	console.log("The message is: " + message);
+});
